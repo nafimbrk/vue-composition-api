6e5f48e87940b518d54d7d741124d70a569b9bee
nafimbrk <nafimubarok2005@gmail.com>
2024-12-26 20:12:40 +0700

    state reactivity

---
+++ src/views/BelajarView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, reactive } from "vue"
+
+const message = ref('hello world')
+const number = ref(0)
+const student = reactive({
+    name: 'jack',
+    age: 9
+})
+
+// reactive gk bisa digunakan untuk tipe data yg primitive (string, boolean, number) bisanya untuk tipe data (object, array, collection)
+
+console.log(message.value)
+
+console.log(student.name)
+
+</script>
+
+<template>
+    <h1>ini halaman belajar view</h1>
+    {{ message }}
+    <br>
+    {{ number }}
+    <br>
+    {{ student.name }}
+    <br>
+    <ul v-for="item in student">
+        <li>{{ item }}</li>
+    </ul>
+</template>
+
+<style scoped></style>
